81f1c78d61d2ed403fbfe7d7b92d8c7fd0864725
Jasper Wong <wlcjasper@gmail.com>
2024-03-25 17:47:40 +0800

    list available attributes

--- src/App1.js
+++ src/App1.js
@@ -28,48 +28,50 @@ export default function App() {
   const handleEndpointChange = (e) => {
     setEndpoint((prev) => e.target.value)
   }
-
   // Display
 
-  const renderData = 
-    input?.data?.map((data) => {
-      if (endpoint === "books") {
-        return <li key={data.id}>{data?.attributes?.title}</li>
-      }
-      if (endpoint === "characters") {
-        return <li key={data.id}>{data?.attributes?.name}</li>
-      }
-      return <li key={data.id}>{data?.id}</li>
-    })
+  const renderData = input?.data?.map((data) => {
+    if (endpoint === "books") {
+      return <li key={data.id}>{data?.attributes?.title}</li>
+    }
+    if (endpoint === "characters") {
+      return <li key={data.id}>{data?.attributes?.name}</li>
+    }
+    return <li key={data.id}>{data?.id}</li>
+  })
 
-  const renderSearchResults = 
-    input?.data?.map((data) => {
-      if (searchTerm === ""){
-        return null
-      }
-      if (
-        endpoint === "books" &&
-        data?.attributes?.title?.includes(searchTerm)
-      ) {
-        return <li key={data.id}>{data?.attributes?.title}</li>
-      }
-      if (endpoint === "characters"
-      &&
-        data?.attributes?.name?.includes(searchTerm)) {
-        return <li key={data.id}>{data?.attributes?.name}</li>
-      }
+  const renderSearchResults = input?.data?.map((data) => {
+    if (searchTerm === "") {
       return null
-    })
+    }
+    if (endpoint === "books" && data?.attributes?.title?.includes(searchTerm)) {
+      return <li key={data.id}>{data?.attributes?.title}</li>
+    }
+    if (
+      endpoint === "characters" &&
+      data?.attributes?.name?.includes(searchTerm)
+    ) {
+      return <li key={data.id}>{data?.attributes?.name}</li>
+    }
+    return null
+  })
 
   return (
     <>
       <h3>Search Potter DB website</h3>
+
       <form onChange={handleEndpointChange}>
         <select>
           <option value="books">Books</option>
           <option value="characters">Characters</option>
         </select>
       </form>
+
+      <p>
+        Available attributes:{" "}
+        {input?.data[0].attributes &&
+          Object.getOwnPropertyNames(input?.data[0].attributes).toString()}
+      </p>
 
       <p className="">
         Search Term{searchTerm && ":"} {searchTerm}
