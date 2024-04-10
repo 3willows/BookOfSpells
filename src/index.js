8c2966ea8f8d713a7d53f968363552756309a179
Jasper Wong <wlcjasper@gmail.com>
2024-04-10 22:38:02 +0800

    test commit with signature

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
-//test signed commits
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
